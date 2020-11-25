2604063b0ef7fb888a7c15593fb1c2c4324eedd9
ashwinbhatt <bhatt.ashwin98@gmail.com>
2020-11-25 17:44:46 +0530

    user delete added

--- routes/pages.js
+++ routes/pages.js
@@ -1,7 +1,7 @@
 const config = process.webConf
 const express = require('express')
 const router = express.Router()
-const { readUser } = require('./db_module')
+const { readUser, deleteUser } = require('./db_module')
 const {checkAuthen} = require('../middlewares/middlewares')
 
 
@@ -15,8 +15,8 @@ router.get(config.app.baseurl + '/login', (req, res) => {
 });
 
 router.get(config.app.baseurl + '/signup', checkAuthen,(req, res) => {
-    if(res.locals.authUser.role != config.admin.username){
-        return res.status(401).json({error: 'You are authorised to add a user !'})
+    if(res.locals.authUser.role != 'admin'){
+        return res.status(401).json({error: 'You are not authorised to add a user !'})
     }
     res.render('signup.ejs', {'url_base': config.app.baseurl})  
 });
@@ -29,7 +29,10 @@ router.get(config.app.baseurl + '/update', checkAuthen,(req, res) => {
     res.render('update.ejs', {'url_base': config.app.baseurl})
 })
 
-router.get(config.app.baseurl + '/delete', (req, res) => {
+router.get(config.app.baseurl + '/delete', checkAuthen,(req, res) => {
+    if(res.locals.authUser.role != 'admin'){
+        return res.status(401).json({error: 'You are not authorised to delete a user !'})
+    }
     res.render('delete.ejs', {'url_base': config.app.baseurl})
 })
 
@@ -38,12 +41,13 @@ router.get(config.app.baseurl + '/user/:username', checkAuthen,(req, res) => {
     if(req.params.username != authUser.username){
         res.redirect(config.app.baseurl+'/login')
     }else{
-        var signUrl=null, updateUser=null; 
+        var signUrl=null, updateUser=null, deleteUser= null; 
         if(authUser.role=='admin'){
             signUrl= config.app.baseurl+'/signup'
             updateUser=config.app.baseurl+'/update'
+            deleteUser=config.app.baseurl+'/delete'
         }
-        res.render('userPage', {'url_base': config.app.baseurl, 'userData': authUser, 'signUrl': signUrl, 'updateUser': updateUser})
+        res.render('userPage', {'url_base': config.app.baseurl, 'userData': authUser, 'signUrl': signUrl, 'updateUser': updateUser, 'deleteUser': deleteUser})
     }
 })
 
